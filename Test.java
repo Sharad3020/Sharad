ff2de3697ad4c587f30992908f2d45277882ce67
Sharad3020 <56599307+Sharad3020@users.noreply.github.com>
2019-10-16 11:27:30 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+package test;
+
+import static java.time.Clock.system;
+
+public class Test {
+    public static void main(String[] args) {      
+
+    /**
+     *
+     * @param str
+     * @return
+     */
+    
+        System.out.println("Hello".substring(1,5));
+System.out.println("Java".substring(1,4));
+System.out.println("Coding".substring(1,6));
+        
+    }
+    
+}
